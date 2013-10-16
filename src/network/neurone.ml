97cc5d27119e72008d7c8abd6a8912ba2e151edd
kuchera <link5525@hotmail.com>
2013-10-16 23:26:35 +0200

    Ajout dans neurones

--- src/network/neurone.ml
+++ src/network/neurone.ml
@@ -1,3 +1,17 @@
 (* Modules Neurone *)
 
-
+class neuron text nb_out =
+    object (this)
+        val label = text
+        val value = 0.
+        val out = Array.make nb_out (0,0.)
+        
+        method reset = value <- 0.
+        method add v = value <- value +. v
+        method add_weight e x =
+            let (o,p) =  out.(e) in
+            out.(e) <- (o,p +. x)
+        method reset_weight e =
+            let (o,p) = out.(e) in
+            out.(e) <- (o,0.)
+    end
