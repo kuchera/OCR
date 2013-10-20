7b67b01043d96825a5dd5809d25c5ef2db183b25
kucher_e <link5525@hotmail.com>
2013-10-20 17:03:03 +0200

    Network -> graph

--- src/network/graph.ml
+++ src/network/graph.ml
@@ -1,6 +1,9 @@
 (* Module Graph *)
 
-class graph param =
+class graph param = (* graph : liste du nombre de neurones par couche en commencant pas la couche la plus basse *)
 object (this)
-	
+	val net =
+		let l = Array.length graph in
+		Array.init l (fun i ->
+			
 end
