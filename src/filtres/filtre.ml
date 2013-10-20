068731269b235ba7c660e9e5b71de8e2ab445137
kucher_e <link5525@hotmail.com>
2013-10-20 03:34:05 +0200

    Organisation

--- src/filtres/filtre.ml
+++ src/filtres/filtre.ml
@@ -1,2 +1,16 @@
- let _ =
-print_string "Not implemented...\n"
+let print_help () =
+		Printf.printf "%s INPUT_IMAGE OUTPUT_IMAGE - Takes away noise and binarises the image.\n%s noise INPUT_FILENAME OUTPUT_FILENAME - Takes away noise.\n%s binarize INPUT_FILENAME OUTPUT_FILENAME - Binarises the image.\n" (Sys.argv.(0)) (Sys.argv.(0)) (Sys.argv.(0))
+	
+let _ = 
+	if Array.length (Sys.argv) >= 4 then
+		if Sys.argv.(1) = "noise" then
+			Bruit.remove_noise (Sys.argv.(1)) (Sys.argv.(2))
+		else if Sys.argv.(1) = "binarize" then
+			Binarisation.binarize (Sys.argv.(1)) (Sys.argv.(2))
+		else
+			print_help ()
+	else if Array.length (Sys.argv) >= 3 then
+		(Bruit.remove_noise (Sys.argv.(1)) (Sys.argv.(2));
+		Binarisation.binarize (Sys.argv.(1)) (Sys.argv.(2)))
+	else
+		print_help ()
