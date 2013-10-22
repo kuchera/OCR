ad83effd20602804d92ee49bb88adacd903a2e35
kuchera <link5525@hotmail.com>
2013-10-22 09:38:42 +0200

    Ajout de commentaires

--- src/filtres/filtre.ml
+++ src/filtres/filtre.ml
@@ -1,5 +1,5 @@
 let print_help () =
-		Printf.printf "%s INPUT_IMAGE OUTPUT_IMAGE - Takes away noise and binarises the image.\n%s noise INPUT_FILENAME OUTPUT_FILENAME - Takes away noise.\n%s binarize INPUT_FILENAME OUTPUT_FILENAME - Binarises the image.\n" (Sys.argv.(0)) (Sys.argv.(0)) (Sys.argv.(0))
+		Printf.printf "%s INPUT_IMAGE OUTPUT_IMAGE - Takes away noise and binarises the image.\n%s noise INPUT_FILENAME OUTPUT_FILENAME - Takes away noise.\n%s binarize INPUT_FILENAME OUTPUT_FILENAME - Binarises the image.\n%s niblack INPUT_FILENAME OUTPUT_FILENAME - Applies the Niblack algorithm.\n" (Sys.argv.(0)) (Sys.argv.(0)) (Sys.argv.(0)) (Sys.argv.(0))
 	
 let _ = 
 	if Array.length (Sys.argv) >= 4 then
--- src/main.ml
+++ src/main.ml
@@ -24,44 +24,23 @@ let display f =
 	let return = Sys.command ("./interface "^f) in
 	Printf.printf "Returned code : %d\n" return
 
-let exec_fil inf outf =
-	Printf.printf "Applying filtres to \"%s\"...\n" inf;
-	let return = Sys.command ("./filtres "^inf^" "^outf) in
+let exec_fil inf outf flags =
+	Printf.printf "Applying filtres %s to \"%s\"...\n" inf flags;
+	let return = Sys.command ("./filtres "^flags^" "^inf^" "^outf) in
 	Printf.printf "Returned code : %d\n" return;
 	if return = 0 then
 		Printf.printf "Image saved in \"%s\".\n" outf
 
-let exec_noise inf outf =
-	Printf.printf "Removing noise from \"%s\"...\n" inf;
-	let return = Sys.command ("./filtres noise "^inf^" "^outf) in
+let exec_rot inf outf flags =
+	Printf.printf "Rotating %s \"%s\"...\n" inf flags;
+	let return = Sys.command ("./rotation "^flags^" "^inf^" "^outf) in
 	Printf.printf "Returned code : %d\n" return;
 	if return = 0 then
 		Printf.printf "Image saved in \"%s\".\n" outf
 
-let exec_bin inf outf =
-	Printf.printf "Binarising \"%s\"...\n" inf;
-	let return = Sys.command ("./filtres binarize "^inf^" "^outf) in
-	Printf.printf "Returned code : %d\n" return;
-	if return = 0 then
-		Printf.printf "Image saved in \"%s\".\n" outf
-
-let exec_nib inf outf =
-	Printf.printf "Binarising \"%s\"...\n" inf;
-	let return = Sys.command ("./filtres niblack "^inf^" "^outf) in
-	Printf.printf "Returned code : %d\n" return;
-	if return = 0 then
-		Printf.printf "Image saved in \"%s\".\n" outf
-
-let exec_rot inf outf =
-	Printf.printf "Rotating \"%s\"...\n" inf;
-	let return = Sys.command ("./rotation "^inf^" "^outf) in
-	Printf.printf "Returned code : %d\n" return;
-	if return = 0 then
-		Printf.printf "Image saved in \"%s\".\n" outf
-
-let exec_det inf outf =
+let exec_det inf outf flags =
 	Printf.printf "Detecting text zones \"%s\"...\n" inf;
-	let return = Sys.command ("./detection draw "^inf^" "^outf) in
+	let return = Sys.command ("./detection "^flags^" "^inf^" "^outf) in
 	Printf.printf "Returned code : %d\n" return;
 	if return = 0 then
 		Printf.printf "Image saved in \"%s\".\n" outf
@@ -78,21 +57,24 @@ try
 		print_string " >";
 		s := read_line ();
 		match !s with
-		| "noise" -> exec_noise !inf !outf
-		| "bin"   -> exec_bin !inf !outf
-                | "nib"   -> exec_nib !inf !outf
-		| "fil"   -> exec_fil !inf !outf
-		| "rot"   -> exec_rot !inf !outf
-		| "det"   -> exec_det !inf (get_entry ())
+		| "noise" -> exec_fil !inf !outf "noise"
+		| "bin"   -> exec_fil !inf !outf "binarise"
+                | "nib"   -> exec_fil !inf !outf "niblack"
+		| "fil"   -> exec_fil !inf !outf ""
+		| "rot"   -> exec_rot !inf !outf ""
+		| "det"   -> exec_det !inf (get_entry ()) "draw"
 		| "dis"   -> display !outf
-		| "c"     -> ignore (Sys.command (get_entry ()))
+                | "rec"   ->    exec_fil !inf !outf "";
+                                exec_rot !inf !outf "";
+                                exec_det !inf (get_entry ()) "";
+                                display !outf
+                | "c"     -> ignore (Sys.command (get_entry ()))
 		| "help"  -> print_file "files/help"
 		| "exit"  -> print "Exiting..."
 		| "in"    -> inf := getinfile ()
 		| "out"   -> outf := getoutfile ()
 		| _       -> print "Type \"help\' for help."
 	done;
-	
 	exit 0
 with e -> 
 	print "The program encountered an error.";
