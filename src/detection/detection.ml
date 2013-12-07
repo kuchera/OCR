54d6bf583d281e80b80397ceffb3e6b2a25f3f71
kucher_e <link5525@hotmail.com>
2013-12-08 00:19:38 +0100

    Ajout d'interface d'appel pour la detection

--- src/detection/detection.ml
+++ src/detection/detection.ml
@@ -1,7 +1,15 @@
 let _ = 
+	(*
 	if Array.length (Sys.argv) == 4 && Sys.argv.(1) = "draw" then
 		ignore (Detect.detect ~draw:true (Sys.argv.(2)) (Sys.argv.(3)))
 	else if Array.length (Sys.argv) == 3 then
 		ignore (Detect.detect (Sys.argv.(1)) (Sys.argv.(2)))
 	else
 		Printf.printf "%s draw IMAGE_NAME OUTPUT_FILE - Draws the histogram and saves the result.\n%s IMAGE_NAME OUTPUT_FILE - Detects characters and saves the result as : x1;y1;w1;h2|x2;y2...\n" (Sys.argv.(0)) (Sys.argv.(0))
+	*)
+	if Array.length Sys.argv == 3 then 
+		Detect.detectChars Sys.argv.(1) Sys.argv.(2)
+	else if Array.length Sys.argv == 2 then
+		Detect.detectChars Sys.argv.(1) "files/network/detected/"
+	else
+		Printf.printf "%s input_file_image output_path - writes each character of the input file to separate files in output_path.\n%s input_filename - the same with path files/network/detected/\n" Sys.argv.(0) Sys.argv.(0)
