7262089c3d40320c6a52f76a77ae5699db7dd65d
kucher_e <link5525@hotmail.com>
2013-12-08 13:49:35 +0100

    Plein de choses

--- src/network/network.ml
+++ src/network/network.ml
@@ -1,8 +1,11 @@
-let dir = "files/network/"
-let file = dir ^ "g1.dat"
+let dirdetect = "files/network/"
 
 let f =
 	(*new Graph.graph 0.5 [|2;2;1|]
 	g#training [|1.;1.|] [|0.|]
 	Printf.printf "%f\n" (g#get_out [|1.;1.|]).(0)*)
-	()
+	let s = Iostream.read_file (dirdetect ^ "detected/1.mat") in
+	let a = Stdlib.string_to_intmat s '|' ';' in
+	Array.iter (fun e ->
+		Array.iter (fun ee -> Printf.printf "%d " ee) e;
+		Printf.printf "\n") a
